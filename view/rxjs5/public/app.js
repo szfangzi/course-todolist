63c6323715cc2b41d64620b02e5b261ef9b1cd02
fangzi <szfangziwei@qq.com>
2017-02-02 21:07:48 +0800

    s

---
+++ view/rxjs5/public/app.js
@@ -0,0 +1,51 @@
+var increaseButton = document.querySelector('#increase');
+var increase = Rx.Observable.fromEvent(increaseButton, 'click')
+  // Again we map to a function the will increase the count
+  .map(() => state => Object.assign({}, state, {
+    count: state.count + 1
+  }));
+
+var decreaseButton = document.querySelector('#decrease');
+var decrease = Rx.Observable.fromEvent(decreaseButton, 'click')
+  // We also map to a function that will decrease the count
+  .map(() => state => Object.assign({}, state, {
+    count: state.count - 1
+  }));
+
+var inputElement = document.querySelector('#input');
+var input = Rx.Observable.fromEvent(inputElement, 'keypress')
+  // Let us also map the keypress events to produce an inputValue state
+  .map(event => state => Object.assign({}, state, {
+    inputValue: event.target.value
+  }));
+
+// We merge the three state change producing observables
+var state = Rx.Observable.merge(
+  increase,
+  decrease,
+  input
+).scan((state, changeFn) => changeFn(state), {
+  count: 0,
+  inputValue: ''
+});
+
+// We subscribe to state changes and update the DOM
+state.subscribe((state) => {
+  document.querySelector('#count').innerHTML = state.count;
+  document.querySelector('#hello').innerHTML = 'Hello ' + state.inputValue;
+});
+
+// To optimize our rendering we can check what state
+// has actually changed
+var prevState = {};
+state.subscribe((state) => {
+  if (state.count !== prevState.count) {
+    document.querySelector('#count').innerHTML = state.count;
+  }
+  if (state.inputValue !== prevState.inputValue) {
+    document.querySelector('#hello').innerHTML = 'Hello ' + state.inputValue;
+  }
+  prevState = state;
+});
+
+[1, 2, 3, 4].map(n => s => console.log(s))
