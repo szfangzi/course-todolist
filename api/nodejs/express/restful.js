dc2b093e1b36cd695accb85dca3f65840e7e0cc1
fang <test@qq.com>
2017-02-17 18:12:32 +0800

    s

--- api/nodejs/express/restful.js
+++ api/nodejs/express/restful.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var md5 = require('md5');
+var multer = require('multer');
 var app = express();
 
 var db = mysql.createConnection({
@@ -79,6 +81,46 @@ app.delete('/todos/:id', function (req, res) {
     }
   });
 });
+
+
+var storage = multer.diskStorage({
+    //设置上传文件路径,以后可以扩展成上传至七牛,文件服务器等等
+    //Note:如果你传递的是一个函数，你负责创建文件夹，如果你传递的是一个字符串，multer会自动创建
+    destination: process.cwd() + '/upload',
+    //TODO:文件区分目录存放
+    //获取文件MD5，重命名，添加后缀,文件重复会直接覆盖
+    filename: function (req, file, cb) {
+        // var fileFormat =(file.originalname).split(".");console.log(fileFormat);
+        cb(null, file.originalname);
+        // cb(null, file.fieldname + '-' + md5(file) + "." + fileFormat[fileFormat.length - 1]);
+    }
+});
+
+//添加配置文件到muler对象。
+var upload = multer({
+    storage: storage,
+    //其他设置请参考multer的limits
+    //limits:{}
+});
+
+
+app.post('/upload', upload.single('file'), function (req, res) {
+    if (req.file) {
+      console.log(req.file);
+      res.send('文件上传成功')
+    }else{
+      res.json({status:0});
+    }
+});
+
+
+
+
+
+
+
+
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
